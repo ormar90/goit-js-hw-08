04e6654aae155911513b3793e75289cf5489a408
Orest Maryniuk <98083499+ormar90@users.noreply.github.com>
2022-08-07 18:46:18 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from "lodash.throttle";
 
+const emailEl = document.querySelector('.feedback-form [type = email]')
+const messageEl = document.querySelector('.feedback-form textarea');
+const feedbackFormEl = document.querySelector('.feedback-form');
+
+const FEEDBACK = "feedback-form-state";
+const formData = {};
+
+feedbackFormEl.addEventListener('submit', onFormSubmit);
+feedbackFormEl.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateTextarea();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(FEEDBACK);
+}
+
+function onTextareaInput(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(FEEDBACK, JSON.stringify(formData));
+}
+
+function populateTextarea(e) {
+    const savedMessage = JSON.parse(localStorage.getItem(FEEDBACK));
+
+    if (savedMessage.email) {        
+        emailEl.value = savedMessage.email;
+    } 
+    if (savedMessage.message) {
+        messageEl.value = savedMessage.message;
+    }
+}
