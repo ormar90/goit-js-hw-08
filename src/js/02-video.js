a01d1851cbaee5f632eb10e74717f1a4f181a8bd
Orest Maryniuk <98083499+ormar90@users.noreply.github.com>
2022-08-07 16:49:25 +0300

    finished second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+const CURRENT_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(function(data) {
+    localStorage.setItem(CURRENT_TIME, JSON.stringify(data.seconds))
+    console.log('timer work');
+}, 1000));
+
+if (localStorage.getItem(CURRENT_TIME)) {
+    player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)));
+}
